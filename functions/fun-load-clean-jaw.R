982e4fb1f6c46a4eb478ecc9ba739b7b80f8108e
Jonathan Whiteley <jawhiteley@me.com>
2016-01-15 13:16:39 -0500

    more cleaning functions from another project

--- functions/fun-load-clean-jaw.R
+++ functions/fun-load-clean-jaw.R
@@ -1,13 +1,14 @@
 ################################################################
 ### General R functions for loading and cleaning data
-### Jonathan A. Whiteley        R v3.2.2        2016-01-05
+### Jonathan A. Whiteley        R v3.2.2        2016-01-15
 ################################################################
 
 ################################################################
 ## LOAD DATA
 
 readWorksheets <- function (wb, sheet, pkg="readxl", ...)
-{
+{ ## Read multiple worksheets into a list of data.frames
+  ## Still a work in progress.
   if (pkg=="openxlsx") {
     require(openxlsx)
     fun <- readWorkbook
@@ -24,7 +25,8 @@ readWorksheets <- function (wb, sheet, pkg="readxl", ...)
 }
 
 read_sheets <- function (wb, sheet, col_types=NULL, ...)
-{
+{ ## Read multiple worksheets into a list of data.frames
+  ## using 'readxl' package.
   require(readxl)
   result <- 
     lapply(sheet, function (.sheet)
@@ -60,4 +62,97 @@ trim <- function(string)
   return( gsub("^\\s+|\\s+$", "", string) )
 }
 
+reforder <- function(vec="", ref="", drop.=FALSE, ...)
+{ ## calculate order of a vector based on a (longer) reference list of the same values.
+  ord <- match(vec, ref)               # match values in the reference vector.
+  ord <- order(ord, ...)               # the order *of the matches* (NA at the end by default)
+  if (drop.) {
+    ## The same effect can be achieved by passing 'na.last=NA' to order(), above.
+    x <- which(!vec %in% ref)          # get indices of missing values    
+    ord <- setdiff(ord, x)             # drop unmatched values
+  }
+  return(ord)
+  if (FALSE) {                         # test code
+    test.vector <- c("#", sample(LETTERS, 10))
+    test.vector
+    test.vector[order(match(test.vector, LETTERS))]
+    test.vector[reforder(test.vector, LETTERS, drop.=FALSE)]
+    test.vector[reforder(test.vector, LETTERS, drop.=TRUE) ]
+    test.vector[reforder(test.vector, rep(LETTERS, each=2) ) ]
+    test.vector2 <- rep(test.vector, 2) 
+    test.vector2[reforder(test.vector2, LETTERS) ]
+  }
+}
+
+vlookup <- function(lookup_value="", table_array=data.frame(), col_index_num=2, col_lookup=1)
+{ ## mimick Excel's vlookup function, for use in calculating values based on lookup tables.
+  lookup.match <- match(lookup_value, table_array[[col_lookup]])
+  ## Extract values from a combination r,c coordinate pairs.
+  ## I feel like theres a faster and simpler vectorized way to do this (and that I've done it before), but this is the best I could come up with.
+  lookup.coords <- cbind(row=lookup.match, col=col_index_num)
+  values <- apply(lookup.coords, 1, function (x)
+                  {
+                    table_array[x['row'], x['col']]
+                  })
+  return( values )
+  if (FALSE) {                         # test code
+    vlookup( c(8:16, NA), data.frame(x=1:length(LETTERS), y=letters, z=LETTERS), rep(c(2, 3), length=length(c(8:16, NA))) )
+  }
+}
+
+
+
+##==============================================================
+## Column and Object Names
+
+convert_names <- function(obj=NULL, new.names="")
+{ ## Convert names of an object using a reference *named* vector,
+  ## where the names are the new names of the object, and the values are the current names of the object, in the desired order.
+  match2new <- match(new.names, names(obj)) 
+  if ( any(is.na(match2new)) )
+    warning("Some new.names not found in object names")
+  names(obj) <- names(new.names)[match2new] # ensure the proper order is maintained
+  if (is.null(dim(obj))) {
+    obj[names(new.names)]
+  } else {
+    obj[names(new.names), ]
+  }
+}
+
+regex_match <- function (patterns="", val="", ...)
+{
+  if ("list" %in% class(patterns))
+  {
+    regex_match.list(patterns=patterns, val=val, ...)
+  } else {
+    regex_match.base(patterns=patterns, val=val, ...)
+  }
+}
+
+regex_match.base <- function (patterns="", val="", fun=grep, ...)
+{ ## run regular expression matching on a list of patterns - until at least one match is found.
+  result.list <- lapply(patterns, fun, val, ...)
+  result.lengths <- sapply(result.list, length)
+  result <- result.list[ which(result.lengths > 0)[1] ]
+  return(result)
+}
+
+regex_match.list <- function(patterns="", val="", fun=grep, ...) 
+{
+  result <- sapply(patterns, function (pat) regex_match.base(pat, val, fun, ...) )
+  return(result)
+}
+
+if (FALSE) {    # Test code
+  test.pats <- COL_NAMES[[1]][1:10]
+  test.vals <- names(test.pats)
+  regex_match(test.pats, test.vals, value=TRUE, perl=TRUE, ignore.case=TRUE)
+  test.pats[[1]] <- ".*"               # ensure duplicates
+  regex_match(test.pats, test.vals, value=TRUE, perl=TRUE, ignore.case=TRUE)
+  regex_match.base(test.pats, test.vals, value=TRUE, perl=TRUE, ignore.case=TRUE)
+}
+
+
+
+##==============================================================
 
